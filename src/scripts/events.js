477bbc1f141f7a56edc8e6770b8f24bf11278534
Laís Rodrigues <devlaisrodrigues@gmail.com>
2025-10-01 07:51:49 -0300

    implements events of the process button and Enter input"

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -1,5 +1,18 @@
 /**
  * Responsabilidad: gestionar los eventos
  * */ 
+import { fizzBuzz } from './fizzBuzz.js';
+import { showResult, clearInput } from './dom.js';
+
+function processNumber() {
+    const number = parseFloat(document.getElementById('numberInput').value);
+    showResult(fizzBuzz(number));
+    clearInput();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('processButton').onclick = processNumber;
+    document.getElementById('numberInput').onkeydown = (event) => event.key === 'Enter' && processNumber();
+});
 
 
