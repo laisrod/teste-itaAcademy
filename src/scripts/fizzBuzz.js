fcd288d5d5289650bbbbe47b2f53eac87368c546
Laís Rodrigues <devlaisrodrigues@gmail.com>
2025-10-01 06:59:23 -0300

    add condition number %5

--- src/scripts/fizzBuzz.js
+++ src/scripts/fizzBuzz.js
@@ -19,5 +19,7 @@ export function fizzBuzz (number) {
     }
     if (number % 3 === 0 && number % 5 === 0) return "FizzBuzz";
     if (number % 3 === 0) return "Fizz";
+    if (number % 5 === 0) return "Buzz";
+    }
 
 }
